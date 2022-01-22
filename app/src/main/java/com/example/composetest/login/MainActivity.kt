b54a991bdf2cbaee28b505f0ae5c497023976977
Boris_Sadakov <veygard@gmail.com>
2022-01-22 14:29:48 +0300

    Пример приложения с авторизацией юзера по данным от ktor сервера: создание юзера, авторизация, смена логина/пароля, получение jwt токена.

---
+++ app/src/main/java/com/example/composetest/login/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.composetest.login
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import com.example.composetest.login.ui.theme.AppTheme
+
+class MainActivity : ComponentActivity() {
+    private lateinit var navController: NavHostController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AppTheme {
+        Greeting("Android")
+    }
+}
