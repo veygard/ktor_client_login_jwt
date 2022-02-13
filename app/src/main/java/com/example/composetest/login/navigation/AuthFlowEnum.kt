ef33b737e635f74eba6ac0b99c27670f3b254121
Boris_Sadakov <veygard@gmail.com>
2022-02-13 13:49:46 +0300

    Удалены лишние части обычной навигации, которые использовались до внедрения библиотеки.

--- app/src/main/java/com/example/composetest/login/navigation/AuthFlowEnum.kt
+++ app/src/main/java/com/example/composetest/login/navigation/AuthFlowEnum.kt
@@ -4,3 +4,12 @@ enum class AuthFlowEnum {
     ChangePassword,
     Registration,
 }
+
+sealed class Screens(val route: String) {
+    object Unauthorized  : Screens("unauthorized_screen")
+    object Home : Screens("home_screen")
+    object Login : Screens("Login_screen")
+    object PhoneEnter : Screens("PhoneEnter_screen")
+    object Otp  : Screens("Otp_screen")
+    object RegisterFinish  : Screens("RegisterFinish_screen")
+}
--- app/src/main/java/com/example/composetest/login/navigation/Navigation.kt
+++
@@ -1,8 +0,0 @@
-package com.example.composetest.login.navigation
-
-import androidx.navigation.NavController
-
-
-fun navigate(navController: NavController, screen:Screens){
-    navController.navigate(screen.route)
-}
